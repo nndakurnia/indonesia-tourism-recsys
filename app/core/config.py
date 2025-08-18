cc675d75c7ee2bbd156e18fd036a0d14a5ed5ed8
nndakurnia <nandaka38@gmail.com>
2025-08-18 10:55:44 +0700

    chore: remove unused files

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-DATA_PATH = BASE_DIR / "data" / "tourism_with_id.csv"
-# MODEL_PATH = BASE_DIR / "data" / "model.json"
-# METADATA_PATH = BASE_DIR / "data" / "metadata.json"
-# PLACES_PATH = BASE_DIR / "data" / "places.json"
-API_VERSION = "1.0.0"
